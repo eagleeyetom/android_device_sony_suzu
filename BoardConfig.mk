e28e355350f9e1b246b498809cc60dce63015047
eagleeyetom <tomek.kondrat@gmail.com>
2018-02-13 22:40:18 +0100

    suzu: TWRP add logcat

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,6 +98,10 @@ TARGET_RECOVERY_DEVICE_MODULES := tzdata
 TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT)/usr/share/zoneinfo/tzdata
 TW_THEME := portrait_hdpi
 
+# Logs
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
 # Workaround for error copying vendor files to recovery ramdisk
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
